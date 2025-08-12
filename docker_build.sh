d48936aa2de5bf7e4416a0e07789fff8c47e69e5
shothogun <otho.tk@hotmail.com>
2025-08-12 09:31:55 -0300

    🎨 Removing no-cache from build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t docker.radiohemp.buzzlabs.com.br/apps/fluffychat:release-prod -f Dockerfile .
+docker build -t docker.radiohemp.buzzlabs.com.br/apps/fluffychat:release-prod -f Dockerfile .
 docker push docker.radiohemp.buzzlabs.com.br/apps/fluffychat:release-prod
